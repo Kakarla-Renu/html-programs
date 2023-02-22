2cc0ae58246e06c61fd7fdfcccad9d1c0a7d0cdd
Kakarla-Renu <118729530+Kakarla-Renu@users.noreply.github.com>
2023-02-22 17:21:00 +0530

    Create location.js

---
+++ location.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>JavaScript Statements</h2>
+<p>A <b>JavaScript program</b> is a list of <b>statements</b> to be executed by a computer.</p>
+<p id="demo"></p>
+<script>
+document.getElementById("demo").innerHTML =
+"The full URL of this pageis: <br>"+window.location.href;  
+</script>
+</body>
+</html>
